8d4c16f96cb5d23ab3a91c38745fed92fa81be99
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-03-09 22:23:34 +0900

    Add `len`

--- src/lib.rs
+++ src/lib.rs
@@ -87,4 +87,20 @@ impl<L, R> M2M<L, R> {
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
+
+    /// Returns the number of pairs in the m2m.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::new();
+    /// assert_eq!(m2m.len(), 0);
+    /// m2m.insert(1, "a");
+    /// assert_eq!(m2m.len(), 1);
+    /// ```
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
 }

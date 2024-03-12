178bdc7c30a8907077d7add6ff4e1927acac31ce
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-03-12 21:57:41 +0900

    Add `Debug` to `SmallM2M`

--- src/smallvec.rs
+++ src/smallvec.rs
@@ -1,3 +1,14 @@
+use core::fmt::{Debug, Formatter, Result};
+
 use smallvec::{Array, SmallVec};
 
 pub struct SmallM2M<A: Array>(SmallVec<A>);
+
+impl<A: Array> Debug for SmallM2M<A>
+where
+    A::Item: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_list().entries(self.0.iter()).finish()
+    }
+}

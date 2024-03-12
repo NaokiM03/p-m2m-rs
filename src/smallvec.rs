98786a6d08d8cab7e31c22bff70d8268928d5ddc
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-03-12 22:02:28 +0900

    Add `FromIterator` to `SmallM2M`

--- src/smallvec.rs
+++ src/smallvec.rs
@@ -1,4 +1,5 @@
 use core::fmt::{Debug, Formatter, Result};
+use core::iter::FromIterator;
 
 use smallvec::{Array, SmallVec};
 
@@ -10,5 +11,17 @@ where
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         f.debug_list().entries(self.0.iter()).finish()
+    }
+}
+
+impl<L: Ord, R: Ord, A: Array<Item = (L, R)>> FromIterator<(L, R)> for SmallM2M<A> {
+    #[inline]
+    fn from_iter<I: IntoIterator<Item = (L, R)>>(iter: I) -> Self {
+        let mut v: SmallVec<A> = iter.into_iter().collect();
+
+        v.sort();
+        v.dedup();
+
+        SmallM2M(v)
     }
 }

dad54ea68c62d5ae62d109a659a117f7d93515ff
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-03-13 00:48:27 +0900

    Add `readonly` feature

--- src/lib.rs
+++ src/lib.rs
@@ -82,6 +82,7 @@ impl<'a, L, R> IntoIterator for &'a M2M<L, R> {
     }
 }
 
+#[cfg(not(feature = "readonly"))]
 impl<'a, L, R> IntoIterator for &'a mut M2M<L, R> {
     type Item = &'a mut (L, R);
     type IntoIter = IterMut<'a, (L, R)>;
@@ -139,74 +140,6 @@ impl<L, R> IntoIterator for M2M<L, R> {
 }
 
 impl<L, R> M2M<L, R> {
-    /// Creates an empty M2M
-    pub fn new() -> M2M<L, R> {
-        Default::default()
-    }
-
-    /// Inserts a left-right pair into the m2m.
-    ///
-    /// If the m2m did not previously contain this pair, `true` is returned.
-    ///
-    /// If the m2m already contained this pair, `false` is returned.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::new();
-    ///
-    /// assert!(m2m.insert(1, "a"));
-    /// assert!(m2m.insert(1, "b"));
-    /// assert!(m2m.insert(2, "a"));
-    /// assert!(m2m.insert(2, "b"));
-    ///
-    /// assert!(!m2m.insert(1, "a"));
-    ///
-    /// assert!(m2m.insert(1, "c"));
-    ///
-    /// let mut iter = m2m.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&(1, "a")));
-    /// assert_eq!(iter.next(), Some(&(1, "b")));
-    /// assert_eq!(iter.next(), Some(&(1, "c")));
-    /// assert_eq!(iter.next(), Some(&(2, "a")));
-    /// assert_eq!(iter.next(), Some(&(2, "b")));
-    /// ```
-    pub fn insert(&mut self, left: L, right: R) -> bool
-    where
-        (L, R): Ord,
-    {
-        let value = (left, right);
-
-        if self.0.contains(&value) {
-            return false;
-        }
-
-        self.0.push(value);
-        self.0.sort();
-
-        true
-    }
-
-    /// Clears the m2m, removing all left-right pairs.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b")]);
-    ///
-    /// assert!(!m2m.is_empty());
-    /// m2m.clear();
-    /// assert!(m2m.is_empty());
-    /// ```
-    pub fn clear(&mut self) {
-        self.0.clear();
-    }
-
     /// Returns `true` if the m2m contains no pairs.
     ///
     /// # Examples
@@ -239,44 +172,6 @@ impl<L, R> M2M<L, R> {
     /// ```
     pub fn len(&self) -> usize {
         self.0.len()
-    }
-
-    /// Removes some pairs from the m2m,
-    /// returning the right values corresponding to the left if the left was previously in the m2m.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a")]);
-    ///
-    /// assert_eq!(m2m.remove(&1), Some(vec!["a"]));
-    /// assert_eq!(m2m.remove(&1), None);
-    ///
-    /// assert!(m2m.is_empty());
-    /// ```
-    pub fn remove(&mut self, left: &L) -> Option<Vec<R>>
-    where
-        L: PartialEq,
-    {
-        let mut rights = Vec::new();
-
-        let mut i = 0;
-        while i < self.0.len() {
-            if &self.0[i].0 == left {
-                let lr = self.0.remove(i);
-                rights.push(lr.1);
-            } else {
-                i += 1;
-            }
-        }
-
-        if rights.is_empty() {
-            return None;
-        }
-
-        Some(rights)
     }
 
     /// Returns `true` if the m2m contains the specified left-right pair.
@@ -321,30 +216,6 @@ impl<L, R> M2M<L, R> {
         self.0.iter()
     }
 
-    /// Returns a mutable iterator.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
-    ///
-    /// m2m.iter_mut().for_each(|(l, _)| *l += 2);
-    ///
-    /// let mut iter = m2m.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&(3, "a")));
-    /// assert_eq!(iter.next(), Some(&(3, "b")));
-    /// assert_eq!(iter.next(), Some(&(4, "a")));
-    /// assert_eq!(iter.next(), Some(&(4, "b")));
-    /// assert_eq!(iter.next(), None);
-    /// ```
-    #[inline]
-    pub fn iter_mut(&mut self) -> IterMut<(L, R)> {
-        self.0.iter_mut()
-    }
-
     /// Extract a slice containing all pairs.
     ///
     /// # Examples
@@ -361,76 +232,6 @@ impl<L, R> M2M<L, R> {
     /// ```
     pub fn as_slice(&self) -> &[(L, R)] {
         self.0.as_slice()
-    }
-
-    /// Extract a mutable slice containing all pairs.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b")]);
-    ///
-    /// let slice = m2m.as_mut_slice();
-    ///
-    /// assert_eq!(slice[0], (1, "a"));
-    /// assert_eq!(slice[1], (1, "b"));
-    ///
-    /// slice[1].0 = 3;
-    ///
-    /// assert_eq!(slice[1], (3, "b"));
-    /// ```
-    pub fn as_mut_slice(&mut self) -> &mut [(L, R)] {
-        self.0.as_mut_slice()
-    }
-
-    /// Retains only the pairs specified by the predicate.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
-    ///
-    /// m2m.retain(|(l, _)| l % 2 == 0);
-    ///
-    /// let mut iter = m2m.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&(2, "a")));
-    /// assert_eq!(iter.next(), Some(&(2, "b")));
-    /// assert_eq!(iter.next(), None);
-    /// ```
-    pub fn retain<F>(&mut self, mut f: F)
-    where
-        F: FnMut(&(L, R)) -> bool,
-    {
-        self.0.retain_mut(|pair| f(pair));
-    }
-
-    /// Rejects the pairs specified by the predicate.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
-    ///
-    /// m2m.reject(|(l, _)| l % 2 == 0);
-    ///
-    /// let mut iter = m2m.iter();
-    ///
-    /// assert_eq!(iter.next(), Some(&(1, "a")));
-    /// assert_eq!(iter.next(), Some(&(1, "b")));
-    /// assert_eq!(iter.next(), None);
-    /// ```
-    pub fn reject<F>(&mut self, mut f: F)
-    where
-        F: FnMut(&(L, R)) -> bool,
-    {
-        self.0.retain_mut(|pair| !f(pair));
     }
 }
 
@@ -484,70 +285,6 @@ impl<L, R> M2M<L, R> {
         let lefts: Vec<&L> = self
             .0
             .iter()
-            .filter(|(_, r)| r == right)
-            .map(|(l, _)| l)
-            .collect();
-
-        if lefts.is_empty() {
-            return None;
-        }
-
-        Some(lefts)
-    }
-
-    /// Returns a mutable reference to the right values corresponding to the left.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, 11), (1, 111), (2, 22), (2, 222)]);
-    ///
-    /// let rights = m2m.get_rights_mut(&1).unwrap();
-    /// rights.into_iter().for_each(|r| *r *= 3);
-    ///
-    /// assert_eq!(m2m.rights(), Some(vec![&22, &33, &222, &333]));
-    /// ```
-    pub fn get_rights_mut(&mut self, left: &L) -> Option<Vec<&mut R>>
-    where
-        L: PartialEq,
-    {
-        let rights: Vec<&mut R> = self
-            .0
-            .iter_mut()
-            .filter(|(l, _)| l == left)
-            .map(|(_, r)| r)
-            .collect();
-
-        if rights.is_empty() {
-            return None;
-        }
-
-        Some(rights)
-    }
-
-    /// Returns a mutable reference to the left values corresponding to the right.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use p_m2m::M2M;
-    ///
-    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
-    ///
-    /// let lefts = m2m.get_lefts_mut(&"a").unwrap();
-    /// lefts.into_iter().for_each(|l| *l *= 3);
-    ///
-    /// assert_eq!(m2m.lefts(), Some(vec![&1, &2, &3, &6]));
-    /// ```
-    pub fn get_lefts_mut(&mut self, right: &R) -> Option<Vec<&mut L>>
-    where
-        R: PartialEq,
-    {
-        let lefts: Vec<&mut L> = self
-            .0
-            .iter_mut()
             .filter(|(_, r)| r == right)
             .map(|(l, _)| l)
             .collect();
@@ -735,5 +472,275 @@ impl<L, R> M2M<L, R> {
         v.sort();
 
         M2M(v)
+    }
+}
+
+#[cfg(not(feature = "readonly"))]
+impl<L, R> M2M<L, R> {
+    /// Creates an empty M2M
+    pub fn new() -> M2M<L, R> {
+        Default::default()
+    }
+
+    /// Inserts a left-right pair into the m2m.
+    ///
+    /// If the m2m did not previously contain this pair, `true` is returned.
+    ///
+    /// If the m2m already contained this pair, `false` is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::new();
+    ///
+    /// assert!(m2m.insert(1, "a"));
+    /// assert!(m2m.insert(1, "b"));
+    /// assert!(m2m.insert(2, "a"));
+    /// assert!(m2m.insert(2, "b"));
+    ///
+    /// assert!(!m2m.insert(1, "a"));
+    ///
+    /// assert!(m2m.insert(1, "c"));
+    ///
+    /// let mut iter = m2m.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&(1, "a")));
+    /// assert_eq!(iter.next(), Some(&(1, "b")));
+    /// assert_eq!(iter.next(), Some(&(1, "c")));
+    /// assert_eq!(iter.next(), Some(&(2, "a")));
+    /// assert_eq!(iter.next(), Some(&(2, "b")));
+    /// ```
+    pub fn insert(&mut self, left: L, right: R) -> bool
+    where
+        (L, R): Ord,
+    {
+        let value = (left, right);
+
+        if self.0.contains(&value) {
+            return false;
+        }
+
+        self.0.push(value);
+        self.0.sort();
+
+        true
+    }
+
+    /// Removes some pairs from the m2m,
+    /// returning the right values corresponding to the left if the left was previously in the m2m.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a")]);
+    ///
+    /// assert_eq!(m2m.remove(&1), Some(vec!["a"]));
+    /// assert_eq!(m2m.remove(&1), None);
+    ///
+    /// assert!(m2m.is_empty());
+    /// ```
+    pub fn remove(&mut self, left: &L) -> Option<Vec<R>>
+    where
+        L: PartialEq,
+    {
+        let mut rights = Vec::new();
+
+        let mut i = 0;
+        while i < self.0.len() {
+            if &self.0[i].0 == left {
+                let lr = self.0.remove(i);
+                rights.push(lr.1);
+            } else {
+                i += 1;
+            }
+        }
+
+        if rights.is_empty() {
+            return None;
+        }
+
+        Some(rights)
+    }
+
+    /// Clears the m2m, removing all left-right pairs.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b")]);
+    ///
+    /// assert!(!m2m.is_empty());
+    /// m2m.clear();
+    /// assert!(m2m.is_empty());
+    /// ```
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    /// Returns a mutable iterator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
+    ///
+    /// m2m.iter_mut().for_each(|(l, _)| *l += 2);
+    ///
+    /// let mut iter = m2m.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&(3, "a")));
+    /// assert_eq!(iter.next(), Some(&(3, "b")));
+    /// assert_eq!(iter.next(), Some(&(4, "a")));
+    /// assert_eq!(iter.next(), Some(&(4, "b")));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    #[inline]
+    pub fn iter_mut(&mut self) -> IterMut<(L, R)> {
+        self.0.iter_mut()
+    }
+
+    /// Extract a mutable slice containing all pairs.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b")]);
+    ///
+    /// let slice = m2m.as_mut_slice();
+    ///
+    /// assert_eq!(slice[0], (1, "a"));
+    /// assert_eq!(slice[1], (1, "b"));
+    ///
+    /// slice[1].0 = 3;
+    ///
+    /// assert_eq!(slice[1], (3, "b"));
+    /// ```
+    pub fn as_mut_slice(&mut self) -> &mut [(L, R)] {
+        self.0.as_mut_slice()
+    }
+
+    /// Retains only the pairs specified by the predicate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
+    ///
+    /// m2m.retain(|(l, _)| l % 2 == 0);
+    ///
+    /// let mut iter = m2m.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&(2, "a")));
+    /// assert_eq!(iter.next(), Some(&(2, "b")));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    pub fn retain<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&(L, R)) -> bool,
+    {
+        self.0.retain_mut(|pair| f(pair));
+    }
+
+    /// Rejects the pairs specified by the predicate.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
+    ///
+    /// m2m.reject(|(l, _)| l % 2 == 0);
+    ///
+    /// let mut iter = m2m.iter();
+    ///
+    /// assert_eq!(iter.next(), Some(&(1, "a")));
+    /// assert_eq!(iter.next(), Some(&(1, "b")));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    pub fn reject<F>(&mut self, mut f: F)
+    where
+        F: FnMut(&(L, R)) -> bool,
+    {
+        self.0.retain_mut(|pair| !f(pair));
+    }
+}
+
+#[cfg(not(feature = "readonly"))]
+impl<L, R> M2M<L, R> {
+    /// Returns a mutable reference to the right values corresponding to the left.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, 11), (1, 111), (2, 22), (2, 222)]);
+    ///
+    /// let rights = m2m.get_rights_mut(&1).unwrap();
+    /// rights.into_iter().for_each(|r| *r *= 3);
+    ///
+    /// assert_eq!(m2m.rights(), Some(vec![&22, &33, &222, &333]));
+    /// ```
+    pub fn get_rights_mut(&mut self, left: &L) -> Option<Vec<&mut R>>
+    where
+        L: PartialEq,
+    {
+        let rights: Vec<&mut R> = self
+            .0
+            .iter_mut()
+            .filter(|(l, _)| l == left)
+            .map(|(_, r)| r)
+            .collect();
+
+        if rights.is_empty() {
+            return None;
+        }
+
+        Some(rights)
+    }
+
+    /// Returns a mutable reference to the left values corresponding to the right.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use p_m2m::M2M;
+    ///
+    /// let mut m2m = M2M::from([(1, "a"), (1, "b"), (2, "a"), (2, "b")]);
+    ///
+    /// let lefts = m2m.get_lefts_mut(&"a").unwrap();
+    /// lefts.into_iter().for_each(|l| *l *= 3);
+    ///
+    /// assert_eq!(m2m.lefts(), Some(vec![&1, &2, &3, &6]));
+    /// ```
+    pub fn get_lefts_mut(&mut self, right: &R) -> Option<Vec<&mut L>>
+    where
+        R: PartialEq,
+    {
+        let lefts: Vec<&mut L> = self
+            .0
+            .iter_mut()
+            .filter(|(_, r)| r == right)
+            .map(|(l, _)| l)
+            .collect();
+
+        if lefts.is_empty() {
+            return None;
+        }
+
+        Some(lefts)
     }
 }

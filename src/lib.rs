681f8adae89fd43c067055b60e573a79397a97e5
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-03-24 09:30:37 +0900

    Add `std` feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+#[cfg(feature = "std")]
+mod stdvec;
+
+#[cfg(feature = "std")]
+pub use stdvec::M2M;
+
 #[cfg(feature = "smallvec")]
 mod smallvec;
-#[cfg(feature = "default")]
-mod stdvec;
 
 #[cfg(feature = "smallvec")]
 pub use smallvec::SmallM2M;
-#[cfg(feature = "default")]
-pub use stdvec::M2M;
